5b9d9a627cd8a9c020d0bef8740c82745019e928
Sean-Paul Mauro <sean-paulmauro@pop-os.localdomain>
2019-10-12 00:04:53 -0400

    Login and registration functionality

---
+++ app/forms.py
@@ -0,0 +1,24 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from app.models import AppUser
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
+    
+class RegistrationForm(FlaskForm):
+    first_name = StringField('First name', validators=[DataRequired()])
+    last_name = StringField('Last name', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Confirm password', validators=[DataRequired(), 
+        EqualTo('password')])
+    submit = SubmitField('Register')
+    
+    def validate_username(self, email):
+        user = AppUser.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different email address.')
--- app/models.py
+++ app/models.py
@@ -1,4 +1,6 @@
-from app import db
+from app import db, login
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 #from sqlalchemy.ext.declarative import declarative_base
 #from sqlalchemy_imageattach.entity import Image, image_attachment
 
@@ -58,7 +60,7 @@ class AwardWinner(db.Model):
     def __repr__(self):
         return '<AwardWinner {}>'.format(self.category_nominee_id)
         
-class AppUser(db.Model):
+class AppUser(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(30))
     last_name = db.Column(db.String(30))
@@ -68,6 +70,12 @@ class AppUser(db.Model):
     
     def __repr__(self):
         return '<User {}>'.format(self.email)
+        
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+        
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
         
 class Pick(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -81,4 +89,7 @@ class Pick(db.Model):
  #   id = db.Column(db.Integer, primary_key=True)
  #   nominee_id = db.Column(db.Integer, db.ForeignKey('nominee_lookup.id'))
     
+@login.user_loader
+def load_user(id):
+    return AppUser.query.get(int(id))    
     
