c38f43a81bada942a02693a123a2f9da686f9cbe
nathanmauro <nathan.e.mauro@gmail.com>
2019-12-28 15:24:32 -0500

    some minor tweaks

--- app/models.py
+++ app/models.py
@@ -3,59 +3,66 @@ from app import db, login
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
+
 class NomineeLookup(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(500), index=True)
     year = db.Column(db.Integer)
-    
+
     def __repr__(self):
         return '<Nominee {}>'.format(self.name)
-        
+
+
 class CategoryLookup(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), index=True)
     points = db.Column(db.Integer)
     notes = db.Column(db.String(2000))
-    
+
     def __repr__(self):
         return '<Category {}>'.format(self.name)
-        
+
+
 class RottenTomatoes(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     link = db.Column(db.String(500))
     consensus = db.Column(db.String(2000))
     score = db.Column(db.String(10))
-    
+
     def __repr__(self):
         return '<RottenTomatoes {}>'.format(self.consensus)
-        
+
+
 class CategoryNominee(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     odds = db.Column(db.Integer)
     category_id = db.Column(db.Integer, db.ForeignKey('category_lookup.id'))
     nominee_id = db.Column(db.Integer, db.ForeignKey('nominee_lookup.id'))
     rt_id = db.Column(db.Integer, db.ForeignKey('rotten_tomatoes.id'))
-    
+
     def __repr__(self):
         return '<CategoryNominee {}>'.format(self.category_id + self.nominee_id)
-        
+
+
 class AwardLookup(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(500), index=True, unique=True)
     boost = db.Column(db.Integer)
     category_id = db.Column(db.Integer, db.ForeignKey('category_lookup.id'))
-    
+
     def __repr__(self):
         return '<AwardLookup {}>'.format(self.name)
-        
+
+
 class AwardWinner(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    category_nominee_id = db.Column(db.Integer, db.ForeignKey('category_nominee.id'))    
+    category_nominee_id = db.Column(db.Integer, db.ForeignKey('category_nominee.id'))
     award_id = db.Column(db.Integer, db.ForeignKey('award_lookup.id'))
-    
+
     def __repr__(self):
         return '<AwardWinner {}>'.format(self.category_nominee_id)
-        
+
+
 class AppUser(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(30))
@@ -66,28 +73,29 @@ class AppUser(UserMixin, db.Model):
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
     admin = db.Column(db.String(10))
-    
+
     def __repr__(self):
         return '<User {}>'.format(self.email)
-        
+
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
-        
+
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
     def is_super_admin(self, user):
         return AppUser.query.get(int(id))
-        
+
+
 class Pick(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    category_nominee_id = db.Column(db.Integer, db.ForeignKey('category_nominee.id'))  
-    app_user_id = db.Column(db.Integer, db.ForeignKey('app_user.id'))  
-    
+    category_nominee_id = db.Column(db.Integer, db.ForeignKey('category_nominee.id'))
+    app_user_id = db.Column(db.Integer, db.ForeignKey('app_user.id'))
+
     def __repr__(self):
         return '<Pick {}>'.format(self.app_user_id + self.category_nominee_id)
-    
+
+
 @login.user_loader
-def load_user(id):
-    return AppUser.query.get(int(id))    
-    
+def load_user(user_id):
+    return AppUser.query.get(int(user_id))
